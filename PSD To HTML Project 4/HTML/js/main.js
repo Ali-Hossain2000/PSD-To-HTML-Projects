80136d40ff93ad3a2efe65909a312a044225a10d
Ali-Hossain2000 <81142834+Ali-Hossain2000@users.noreply.github.com>
2022-02-15 21:34:12 +0600

    Add files via upload

---
+++ PSD To HTML Project 4/HTML/js/main.js
@@ -0,0 +1,9 @@
+// BX Slider
+
+$(document).ready(function(){
+    $('.slider_area_items').bxSlider();
+    $('.menu_area').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    });
+  });
